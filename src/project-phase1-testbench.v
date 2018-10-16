1633fe538e559f518569ed93f37c40e7fb2539b8
Frank Hong <frankied4c@gmail.com>
2018-10-16 15:50:26 -0500

    fix typo

--- src/project-phase1-testbench.v
+++ src/project-phase1-testbench.v
@@ -153,7 +153,7 @@ module cpu_tb();
 //   assign PC = DUT.fetch0.pcCurrent; //You won't need this because it's part of the main cpu interface
    assign Inst = DUT.IMEMO.data_out;
 
-   assign RegWrite = DUT.Control.WriteReg;
+   assign RegWrite = DUT.Control.Regwrite;
    // Is memory being read, one bit signal (1 means yes, 0 means no)
 
    assign WriteRegister = DUT.Regi.DstReg;
