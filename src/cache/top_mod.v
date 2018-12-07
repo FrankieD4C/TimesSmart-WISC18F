813ee958cc449a9807eae0d857bb4c0590a343ae
WAXIOR <44177310+wacciorchen@users.noreply.github.com>
2018-12-07 00:20:46 -0600

    need a little bit change
    add several pipe stage

--- src/cache/top_mod.v
+++ src/cache/top_mod.v
@@ -19,38 +19,46 @@ module top_mod(input [15:0] pc_addr, input [15:0] data_addr, input [15:0] data_i
 	wire [15:0] I_addr_in, D_addr_in, D_data_in, I_data_in;
 	wire[15:0] memo_addr, memo_data_out, write_addr;
 	wire [1:0] State;
-	reg [1:0] Next_state; // 00:idle; 01: I miss; 10: D miss; 11 Ddone wait state, wait for writting data
+	reg [1:0] NNext_state; // 00:idle; 01: I miss; 10: D miss; 11 Ddone wait state, wait for writting data
+	wire [1:0] Next_state;
 	wire miss_signal;
 
 
 	wire D_cache_en;
 	assign D_cache_en = (MEM_read | MEM_write) ? 1 : 0;
-
+//******************************************************First Stage*****************************************************//
+	wire int_I_cache_miss, int_D_cache_miss;
 	always @* begin
-		casex({I_cache_miss, D_cache_miss, State, MEM_write, tag_en})
-		default: Next_state = 2'b00;
-		6'b0_0_00_?_?: Next_state = 2'b00;
-		6'b1_0_00_?_?: Next_state = 2'b01;
-		6'b1_?_01_?_0: Next_state = 2'b01; // if not done, remain state
-		6'b?_1_10_?_0: Next_state = 2'b10;
-		6'b?_1_00_?_?: Next_state = 2'b10;//if D miss, always take D first
-		6'b0_0_01_?_1: Next_state = 2'b00; // if only I miss, when done, back to idle, tag_en means done
-		6'b0_0_10_?_1: Next_state = 2'b00;
-		6'b1_0_10_1_1: Next_state = 2'b11; // when MEM_write for D, if I miss, go to state Wait 2'b11
-		6'b1_?_11_?_?: Next_state = 2'b01; // wait one cycle and then I miss state
-		6'b0_1_01_?_1: Next_state = 2'b10;
+		casex({I_cache_miss, D_cache_miss, Next_state, MEM_write, tag_en})
+		default: NNext_state = 2'b00;
+		6'b0_0_00_?_?: NNext_state = 2'b00;
+		6'b1_0_00_?_?: NNext_state = 2'b01;
+		6'b1_?_01_?_0: NNext_state = 2'b01; // if not done, remain state
+		6'b?_1_10_?_0: NNext_state = 2'b10;
+		6'b?_1_00_?_?: NNext_state = 2'b10;//if D miss, always take D first
+		6'b0_0_01_?_1: NNext_state = 2'b00; // if only I miss, when done, back to idle, tag_en means done
+		6'b0_0_10_?_1: NNext_state = 2'b00;
+		6'b1_0_10_1_1: NNext_state = 2'b11; // when MEM_write for D, if I miss, go to state Wait 2'b11
+		6'b1_?_11_?_?: NNext_state = 2'b01; // wait one cycle and then I miss state
+		6'b0_1_01_?_1: NNext_state = 2'b10;
 		endcase
 	end
 
-	dff MEMOD[1:0](.q(State[1:0]), .d(Next_state[1:0]), .wen(1'b1), .clk(clk), .rst(rst_n));
-	assign miss_signal = (I_cache_miss | D_cache_miss) ? 1 : 0;
+	dff MEMOD[1:0](.q(Next_state[1:0]), .d(NNext_state[1:0]), .wen(1'b1), .clk(clk), .rst(rst_n));
+//*************************************************************First Stage*************************************************//
+
+//***************************************************************Second Stage**********************************************//
+	dff MEMOF[1:0](.q(State[1:0]), .d(Next_state[1:0]), .wen(1'b1), .clk(clk), .rst(rst_n)); // modified part
+	assign miss_signal = (Next_state == 2'b01 | Next_state == 2'b10) ? 1 : 0;
 	wire [15:0] miss_addr;
 	wire int_memo_en;
 	assign miss_addr = (Next_state == 2'b10) ? data_addr : (Next_state == 2'b01) ? pc_addr : 16'b0; // use state will delay oen cycle, use miss signal is better
 
 	cache_fill_FSM FSM(.clk(clk), .rst_n(rst_n), .miss_detected(miss_signal), .miss_address(miss_addr), .fsm_busy(busy), .write_data_array(array_en),
- .write_tag_array(tag_en), .memory_address(memo_addr), .memory_data(), .memory_data_valid(data_va), .memory_enable(int_memo_en),  .write_address(write_addr));
-
+ .write_tag_array(tag_en), .memory_address(memo_addr), .memory_data(), .memory_data_valid(data_va), .memory_enable(int_memo_en), .write_address(write_addr));//write addresswrite_addr
+//***************************************************************Second Stage***********************************************//
+	dff IDCMIS[1:0] (.q({int_I_cache_miss, int_D_cache_miss}), .d({I_cache_miss, D_cache_miss}), .wen(1'b1), .clk(clk), .rst(rst_n));
+	// delay one cycle for FSM related signal
 	//output logic
 	assign D_array_en = (State == 2'b10) ? array_en : 0;
 	assign D_tag_en = (State == 2'b10) ? tag_en : 0;
@@ -59,22 +67,22 @@ module top_mod(input [15:0] pc_addr, input [15:0] data_addr, input [15:0] data_i
 
 
 	//miss? 1 miss, 0 hit
-	assign D_addr_in = (D_cache_miss) ? write_addr : data_addr; // miss? addr from FSM, else from pipeline
-	assign D_data_in = (D_cache_miss) ? memo_data_out : data_in;
+	assign D_addr_in = (int_D_cache_miss) ? write_addr : data_addr; // miss? addr from FSM, else from pipeline
+	assign D_data_in = (int_D_cache_miss) ? memo_data_out : data_in;
 
 	D_cache DCT(.addr_input(D_addr_in), .data_input(D_data_in), .data_output(D_output),
 	.write_inputdata(MEM_write), .write_data_en(D_array_en), .write_tag_en(D_tag_en),
 	.clk(clk), .rst_n(rst_n), .MEM_stall(D_cache_miss), .D_en(D_cache_en));
 
-	assign I_addr_in = (I_cache_miss) ? write_addr : pc_addr; // miss? addr from FSM, else from pipeline
-	assign I_data_in = (I_cache_miss) ? memo_data_out : 16'h0;	// take care for this data_in effect
+	assign I_addr_in = (int_I_cache_miss) ? write_addr : pc_addr; // miss? addr from FSM, else from pipeline
+	assign I_data_in = (int_I_cache_miss) ? memo_data_out : 16'h0;	// take care for this data_in effect
 
 	I_cache ICT(.addr_input(I_addr_in), .data_input(memo_data_out), .data_output(I_output),
 	.write_inputdata(1'b0), .write_data_en(I_array_en), .write_tag_en(I_tag_en),
 	.clk(clk), .rst_n(rst_n), .IF_stall(I_cache_miss));
 
 	wire memo_en;
-	assign memo_en = ((D_cache_miss | I_cache_miss | MEM_write)& int_memo_en) ? 1 : 0;
+	assign memo_en = ((int_D_cache_miss | int_I_cache_miss | MEM_write)& int_memo_en) ? 1 : 0;
 	//concern about read miss & write to memory simutaneously ( memory code)
 	memory4c MEMO(.data_out(memo_data_out), .data_in(data_in), .addr(memo_addr), .enable(memo_en), .wr((~D_cache_miss & MEM_write)), .clk(clk), .rst(~rst_n), .data_valid(data_va));
 
